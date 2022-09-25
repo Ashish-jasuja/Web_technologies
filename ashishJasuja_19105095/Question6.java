b9f78e2b636a441bc111a1524c5e8b6ed969ef81
Ashish-jasuja <65953666+Ashish-jasuja@users.noreply.github.com>
2022-09-25 22:53:21 +0530

    Add files via upload

---
+++ ashishJasuja_19105095/Question6.java
@@ -0,0 +1,10 @@
+public class Question6 {
+    public static void main(String args[]){
+        int a=3;
+        do{
+            System.out.print(a+" ");
+            a+=3;
+        }
+        while(a<=36);
+    }
+}
